c7e1ea8845cc95aab3e131482dc37d7242679b60
Павлов Денис Валерьевич <dvpavl23@mts.ru>
2024-04-18 17:52:08 +0700

    ревью

--- ConsoleCustomerList/src/CustomerStorage.java
+++ ConsoleCustomerList/src/CustomerStorage.java
@@ -1,46 +1,41 @@
 import java.util.HashMap;
+import java.util.Map;
 
-public class CustomerStorage
-{
-    private HashMap<String, Customer> storage;
+public class CustomerStorage {
+    private Map<String, Customer> storage; // используй интерфейсы когда объявляешь типы объектов
 
-    public CustomerStorage()
-    {
+    public CustomerStorage() {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
         String[] components = data.split("\\s+");
-        if(components.length != 4){
+        if (components.length != 4) {
             throw new IllegalArgumentException("Неверный формат команды." +
                     "Правильный формат:dd Василий Петров vasily.petrov@gmail.com +79215637722");
         }
         String name = components[0] + " " + components[1];
         char[] charArray = components[3].toCharArray();
-        if(charArray[0] != '+' || charArray[1] != '7' || charArray[2] != '9' || charArray.length != 12){
-            throw new IllegalArgumentException
-                    ("Неверный формат номера телефона.Правильный формат: +79215637722");
+        if (charArray[0] != '+' || charArray[1] != '7' || charArray[2] != '9' || charArray.length != 12) {
+            throw new IllegalArgumentException("Неверный формат номера телефона.Правильный формат: +79215637722");
         }
-        if(!components[2].matches("^[\\w-\\.]+@[\\w-]+(\\.[\\w]+)*\\.[a-z]{2,}$")){
-            throw new IllegalArgumentException
-                    ("Некоректный ввод mail");
+        if (!components[2].matches("^[\\w-\\.]+@[\\w-]+(\\.[\\w]+)*\\.[a-z]{2,}$")) {
+            throw new IllegalArgumentException("Некоректный ввод mail");
         }
         storage.put(name, new Customer(name, components[3], components[2]));
+        // components[3], components[2] - такие вызовы лучше делать единожды и создавать на них переменные.
+        // будет проще использовать типа: String phone = components[3]
     }
 
-    public void listCustomers()
-    {
+    public void listCustomers() {
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
         storage.remove(name);
     }
 
-    public int getCount()
-    {
+    public int getCount() {
         return storage.size();
     }
 }
--- ConsoleCustomerList/src/Main.java
+++ ConsoleCustomerList/src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     private static String addCommand = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static String commandExamples = "\t" + addCommand + "\n" +
@@ -10,35 +9,30 @@ public class Main
             commandExamples;
     private static String helpText = "Command examples:\n" + commandExamples;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
-        for(;;)
+        for (; ; ) // можно использовать while(условие) - https://javarush.com/groups/posts/1876-operator-while
         {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
+            String token = tokens[0]; // можно вынести в переменную и потом везде с ней работать и не делать каждый раз tokens[0]
+
+            if (token.equals("add")) {
                 try {
                     executor.addCustomer(tokens[1]);
-                } catch (IllegalArgumentException ex){
+                } catch (IllegalArgumentException ex) {
                     System.out.println(ex.getMessage());
                 }
-            }
-            else if(tokens[0].equals("list")) {
+            } else if (tokens[0].equals("list")) {
                 executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
-            {
+            } else if (tokens[0].equals("remove")) {
                 executor.removeCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("count")) {
+            } else if (tokens[0].equals("count")) {
                 System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
+            } else if (tokens[0].equals("help")) {
                 System.out.println(helpText);
-            }
-            else {
+            } else {
                 System.out.println(commandError);
             }
 
